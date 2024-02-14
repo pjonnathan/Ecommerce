0680d688cff475e263a0ed55fd782050a4365086
pjonnathan <jonnathanmatos846@gmail.com>
2024-02-14 14:06:38 -0300

    feture nova

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,18 @@
+import { useContext } from "react"
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from "react-router-dom";
+import Breadcrum from "../componets/Breadcrums/Breadcrum";
+import ProductDisplay from "../componets/ProductDisplay/ProductDisplay";
+
 const Product = () => {
+    const { all_product } = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_product.find((e) => e.id === Number(productId));
     return (
-        <div>Product</div>
+        <div>
+            <Breadcrum product={product} />
+            <ProductDisplay product={product} />
+        </div>
     )
 }
 
