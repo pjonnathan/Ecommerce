59ee7825557bf14ae5dfa457558836b7580cee9f
pjonnathan <jonnathanmatos846@gmail.com>
2024-02-18 12:21:10 -0300

    projeto final

---
+++ src/componets/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptiobox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nulla error reiciendis voluptatem qui commodi, rem ab quisquam inventore aspernatur adipisci
+                    deleniti! Minus, quaerat sunt! Aspernatur cupiditate impedit nobis alias, veniam, eius sequi dolorum et, cum enim est? Blanditiis ipsa alias tempora voluptatem
+                    nesciunt quibusdam magnam fugit eius libero nulla sit ab voluptate similique doloribus quidem dolorem vitae assumenda non aliquam odit atque, rem, odio quos. Maxime eos
+                    laboriosam sit asperiores cupiditate numquam blanditiis doloribus voluptate ea nam quisquam repudiandae id sint velit.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi rerum repellat modi. Sint, sunt veritatis. Hic provident laudantium, veritatis explicabo corrupti deleniti id, distinctio modi
+                    voluptates iure, placeat eligendi amet obcaecati sunt molestias in illum harum ea. Ea assumenda ipsam quod distinctio hic maxime veritatis minus. Sint inventore natus sapiente earum.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
--- src/componets/navbar/navbar.jsx
+++ src/componets/navbar/navbar.jsx
@@ -1,7 +1,12 @@
 import './navbar.css'
 import cartIcon from '../../assets/saco.png';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { ShopContext } from '../../Context/ShopContext';
 const Navbar = () => {
+
+    const { getTotalCartItems } = useContext(ShopContext);
+
     return (
         <div className="navbar">
             <h1>FASCO</h1>
@@ -12,7 +17,7 @@ const Navbar = () => {
                 <li className='nav'><Link style={{ color: 'black' }} to='/kids'>Kids</Link></li>
                 <li className='nav'>
                     <Link to='/cart'><img className='img-nav' src={cartIcon} alt="" /></Link>
-                    <div className="nav-cart-cont">0</div>
+                    <div className="nav-cart-cont">{getTotalCartItems()}</div>
                 </li>
                 <li className='signup'><Link style={{ color: 'white' }} to='/LoginSignup'>Login</Link></li>
             </ul>
